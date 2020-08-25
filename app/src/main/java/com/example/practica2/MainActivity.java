acc5b1462e787514a3bf98c92fabe357b2afa188
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2020-08-25 12:11:19 -0500

    Ahi voy

--- app/src/main/java/com/example/practica2/MainActivity.java
+++ app/src/main/java/com/example/practica2/MainActivity.java
@@ -18,13 +18,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button buttonA;
     private TextView puntajeA;
     private TextView contador;
+    private Button BIntentar;
 
     private ArrayList<Pregunta> preguntas;
 
     private int num1, num2, operacion;
     private int contadorsito;
     private int puntaje;
-    private boolean activarContador;
+    private boolean activarContador,activarPuntaje, bueno;
+
 
 
     @Override
@@ -37,11 +39,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonA = findViewById(R.id.buttonA);
         puntajeA = findViewById(R.id.puntajeA);
         contador = findViewById(R.id.contador);
+        BIntentar=findViewById(R.id.BIntentar);
         preguntas = new ArrayList<Pregunta>();
-
-        contadorsito = 20;
+        puntaje=0;
+        contadorsito = 10;
         activarContador = true;
-        puntaje = 0;
+        activarPuntaje=false;
+
+        BIntentar.setVisibility(View.GONE);
+
+
+
 
         nextQuestion();
 
@@ -54,9 +62,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Confirmacion respuesta del usuario
         buttonA.setOnClickListener(this);
 
+        BIntentar.setOnClickListener(
+                (v)->{
+                    nextQuestion();
+
+
+                }
+        );
+
+
     }
 
-        //este es el contador pero no se donde ponerlo aun, y no funciona bien :/
+
+    @Override
+    public void onClick(View view) {
+
+       // puntajeA.setText("Puntaje: " + puntaje);
+        String laRespuesta = answerA.getText().toString();
+
+        //edito puntaje
+        puntajeA.setText("Puntaje: " + puntaje);
+
+        for (int i = 0; i < preguntas.size(); i++) {
+
+            if (laRespuesta.equals(Integer.toString(preguntas.get(i).getSolucion()))) {
+                puntaje += 10;
+                nextQuestion();
+                boolean correcto=true;
+
+
+
+            }
+
+
+            /*else {
+                puntaje -= 2;
+            }*/
+
+
+            //Log.e("prueba" , ""+preguntas.get(i).getSolucion());
+            Log.e("prueba" , ""+puntaje);
+        }
+    }
+
+
+    //este es el contador pero no se donde ponerlo aun, y no funciona bien :/
 
         public void tiempo () {
 
@@ -66,6 +116,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         while (activarContador) {
                             contadorsito--;
                             runOnUiThread( () ->contador.setText(" " + contadorsito));
+
+                            if (contadorsito==0){
+                                puntaje-=2;
+                                activarContador=false;
+
+                                //asi aparece el otro boton
+                                runOnUiThread( () ->  BIntentar.setVisibility(View.VISIBLE));
+
+                            }
+
                             try {
                                 Thread.sleep(1000);
                             } catch (InterruptedException e) {
@@ -77,41 +137,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
 
-        
-
-    @Override
-    public void onClick(View view) {
-
-        String laRespuesta = answerA.getText().toString();
-        puntajeA.setText("Puntaje:" + puntaje);
-
-        for (int i = 0; i < preguntas.size(); i++) {
-
-            if (laRespuesta.equals(Integer.toString(preguntas.get(i).getSolucion()))) {
-
-                puntaje += 15;
-                nextQuestion();
-
-
-            } else {
-
-                puntaje -= 2;
-
-
-            }
-
-
-            Log.e("prueba" , ""+preguntas.get(i).getSolucion());
-        }
-    }
 
 
 
     public void nextQuestion(){
 
-        num1 = (int) (Math.random() * 10 + 1);
-        num2 = (int) (Math.random() * 10 + 1);
-        operacion = (int) (Math.random() * 4 + 1);
+        num1 = (int) Math.floor((Math.random() * 10 + 1));
+        num2 = (int) Math.floor((Math.random() * 10 + 1));
+        operacion = (int) Math.floor((Math.random() * 5 + 1));
         preguntas.add(new Pregunta(num1, num2, operacion));
 
         for (int i = 0; i < preguntas.size(); i++) {
@@ -120,6 +153,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+
+
 
 
 
